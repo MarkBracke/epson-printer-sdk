d4152cfcb388761b02b9e5a86854326b9df1ef25
mark.bracke@tactics.be <mark.bracke@tactics.be>
2025-02-21 11:35:43 +0100

    Prettified constructor; useSsl now uses the default value false in stead of potentially null

--- src/printer.ts
+++ src/printer.ts
@@ -10,9 +10,8 @@ const EPSON_XML_HEADER =
 class EpsonPrinter {
   private url;
 
-  constructor(ip: string, useSsl?: boolean)
-  {
-	  this.url = `${useSsl == true ? 'https' : 'http'}://${ip}/cgi-bin/epos/service.cgi?devid=local_printer&timeout=10000`
+  constructor(ip: string, useSsl = false) {
+	  this.url = `${useSsl ? 'https' : 'http'}://${ip}/cgi-bin/epos/service.cgi?devid=local_printer&timeout=10000`
   }
 
   public async send(print: EpsonPrint) {
